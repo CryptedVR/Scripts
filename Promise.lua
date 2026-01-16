ec5ec13008d873b8d918f2d64a220349cd1624b9
Crypted <41970118+CryptedVR@users.noreply.github.com>
2026-01-16 22:32:27 +0100

    Update Promise.lua

--- Promise.lua
+++ Promise.lua
@@ -1,16 +1,27 @@
-return function <Result>(Try :() -> (Result)) :(boolean, Result)
-	local Tries :number, Success :boolean, Result :any? = 0, false, nil;
+--- VARIABLES
+local M = {};
+
+--- SETTINGS
+M.Retry = 1; -- Time between tries
+M.Tries = 5;
+
+--- FUNCTIONS
+M.Promise = function <Result>(Try :() -> (Result)) :(boolean, Result)
+	local Tries :number, Success :boolean, Result :any? = M.Tries, false, nil;
 
 	while true do
-		Tries += 1;
+		Tries -= 1;
 		Success, Result = pcall(Try);
 
-		if Success or Tries >= 5 then
+		if Success or Tries <= 0 then
 			break
 		else
-			task.wait(1);
+			task.wait(M.Retry);
 		end;
 	end;
 
 	return Success, Result;
 end;
+
+--- MODULE
+return M;
